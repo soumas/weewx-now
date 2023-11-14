89680af01ff3b43cbbe5278daf067b0f733e9591
Thomas Juen <office@soumasoft.com>
2023-11-14 21:01:16 +0100

    convert service to generator

---
+++ weewx_extension/bin/user/image_list_generator.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Thomas Juen
+# Distributed under the terms of GPLv3
+
+"""
+Generator for weewx that generates an index file of available
+images in json format for usage in the weewx web app.
+"""
+
+#import os
+#import os.path
+#import time
+
+#import weewx
+#import weewx.engine
+#import weeutil.weeutil
+
+#VERSION = "0.1"
+
+# ... example: https://github.com/matthewwall/weewx-sftp/blob/master/bin/user/sftp.py
