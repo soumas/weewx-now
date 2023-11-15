b0c8dea471d147f22deefc3dc74850bef12510a3
Thomas Juen <office@soumasoft.com>
2023-11-15 22:51:14 +0100

    installer & imagegenerator, fonts & co.

--- webapp-extension/install.py
+++ webapp-extension/install.py
@@ -29,6 +29,28 @@ class WebappInstaller(ExtensionInstaller):
                     }
                 }
             },
-            files=[('bin/user', ['bin/user/webapp.py']),
-                   ('skins/webapp', ['skins/webapp/skin.conf', 'skins/webapp/config.json.tmpl'])]
+            files=[('bin/user', [
+                'bin/user/webapp.py'
+                ]),
+                   ('skins/webapp', [
+                'skins/webapp/skin.conf', 
+                'skins/webapp/config.json.tmpl',
+                'skins/webapp/lang/cn.conf',
+                'skins/webapp/lang/cz.conf',
+                'skins/webapp/lang/de.conf',
+                'skins/webapp/lang/en.conf',
+                'skins/webapp/lang/es.conf',
+                'skins/webapp/lang/fr.conf',
+                'skins/webapp/lang/gr.conf',
+                'skins/webapp/lang/it.conf',
+                'skins/webapp/lang/nl.conf',
+                'skins/webapp/lang/no.conf',
+                'skins/webapp/lang/th.conf',
+                'skins/webapp/font/Kanit-Bold.ttf',
+                'skins/webapp/font/Kanit-Regular.ttf',
+                'skins/webapp/font/OpenSans-Bold.ttf',
+                'skins/webapp/font/OpenSans-Regular.ttf',
+                'skins/webapp/font/OpenSans.woff',
+                'skins/webapp/font/OpenSans.woff2'
+                ])]
             )
