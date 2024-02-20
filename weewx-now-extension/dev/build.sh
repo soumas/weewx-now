4f53c27c5347afdcd74f357fb3f08dee8bf2b8f7
Thomas Juen <office@soumasoft.com>
2024-02-20 23:36:48 +0100

    rename

--- weewx-now-extension/dev/build.sh
+++ weewx-now-extension/dev/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ..
-rm weewx-now.zip
-zip -r weewx-now.zip weewx-now -x weewx-now/dev/\* weewx-now/dev/weewx-now.zip
+rm weewx-now-extension.zip
+zip -r weewx-now-extension.zip weewx-now-extension -x weewx-now-extension/dev/\* weewx-now-extension/dev/weewx-now-extension.zip
 sudo weectl extension uninstall now --yes
-sudo weectl extension install weewx-now.zip --yes
+sudo weectl extension install weewx-now-extension.zip --yes
 sudo weectl report run
