ee9afb76b6b140901bd9720f2c88a8a87508ab82
Thomas Juen <office@soumasoft.com>
2024-06-06 22:21:52 +0200

    fix js bug

--- weewx-now-extension/skins/now/js/weewx-now.js
+++ weewx-now-extension/skins/now/js/weewx-now.js
@@ -9,3 +9,30 @@ function setChartHours(hours) {
     chartTempPressure(dataset);
     chartRain(dataset);
 }
+
+
+async function initImages(maxImageCount) {
+      
+    const imageData = await loadJsonObj('nowImageIndex.json');
+
+    const splide = new Splide('.splide', {
+      direction: 'rtl'}).mount();
+
+    imageData.images.sort(compareDate);
+    for(i = 0; i < maxImageCount; i++) {
+      var tmplHtml = document.getElementById('slide-template').innerHTML;
+      tmplHtml = tmplHtml.replace('%img%', 'images/webcam/' + imageData.images[i].data);
+      tmplHtml = tmplHtml.replace('%timestamp%', new Date(imageData.images[i].date * 1000).toLocaleString());
+      splide.add(tmplHtml);
+    }
+}
+
+function compareDate(a,b) {
+    if ( a.date < b.date ){
+      return 1;
+    }
+    if ( a.date > b.date ){
+      return -1;
+    }
+    return 0;
+}
